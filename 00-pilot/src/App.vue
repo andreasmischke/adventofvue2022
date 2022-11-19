01fd83a85ba086456e5269ca6c7a46796c69c4f0
Andreas Mischke <andreas.mischke@prym.com>
2022-11-20 00:29:14 +0100

    Initialize repo with template for pilot puzzle

---
+++ 00-pilot/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const X = 'x'
+const O = 'o'
+
+const state = reactive({
+  field: [reactive([null, X, null]), reactive([null, O, null]), reactive([null, null, null])],
+})
+
+console.log('debug', state.field[2].value)
+</script>
+
+<template>
+  <div class="w-full h-full flex justify-center items-center bg-red-500">
+    <table>
+      <tbody>
+        <tr v-for="row in 3" :key="row">
+          <td v-for="col in 3" :key="col" class="bg-slate-500">{{ state.field[row][col] }}: {{ row }}#{{ col }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
