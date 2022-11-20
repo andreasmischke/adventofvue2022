503d355c08faa3f41fe2bcede0897b0c29cf0e92
Andreas Mischke <andreas.mischke@prym.com>
2022-11-20 21:22:23 +0100

    create build script and jekyll generated index page

---
+++ build.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+indexFile="./docs/index.html"
+directories="$(ls -1 ./ | grep -e '^\d\d')"
+
+echo "---" > $indexFile
+echo "days:" >> $indexFile
+
+for dir in $directories; do
+    echo "  - $dir" >> $indexFile
+    pushd $dir;
+    npm run build -- --outDir=../docs/$dir --emptyOutDir --base=/adventofvue2022/$dir/ .
+    popd;
+done;
+
+echo "---" >> $indexFile
